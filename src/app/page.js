576e5987603b2bf2df77c7b54599343ce0730040
Abhishek Kumar <abhi.kumar252004@gmail.com>
2025-02-10 12:24:42 +0530

    al

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
-import Image from "next/image";
+
   import Header from "@/Components/Header";
   import Hero from "@/Components/Hero";
+
 export default function Home() {
   return (
     <div className="">
